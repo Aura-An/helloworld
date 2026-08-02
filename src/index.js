40c071867bb8f185e1969e0c8258235c4b415417
ayr <87597493@qq.com>
2026-08-02 12:30:48 +0800

    12121222

--- src/index.js
+++ src/index.js
@@ -3,26 +3,6 @@ import * as THREE from 'three';
 import mesh from './model.js'
 
 let camera, scene, renderer;
-init();
-
-
-function init() {
-  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 100 );
-  camera.position.z = 2;
-  //222222222222222222222222222
-  scene = new THREE.Scene();
-  scene.add( mesh ); // 2232
-
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.setAnimationLoop( animate );
-  document.body.appendChild( renderer.domElement );
-
-
-  window.addEventListener( 'resize', onWindowResize );
-
-}
 
 function onWindowResize() {
 
